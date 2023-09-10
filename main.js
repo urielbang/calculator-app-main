ad47e86cd1188d110b5896186d9034b19b4492ca
urielbang <uri32655@gmail.com>
2023-09-10 15:08:04 +0300

    done

--- main.js
+++ main.js
@@ -1,10 +1,48 @@
-var num2 = [5, 6, 3, 9, 7, 8, 3, 0];
 var collectionButtons = document.querySelectorAll("button");
 
 var calcDisplay = document.getElementById("displayCalc");
 
+var currentValue = "";
+var previousValue = "";
+
 collectionButtons.forEach((button) => {
   button.addEventListener("click", function () {
-    var value = button.innerHTML;
+    var value = button.textContent;
+    if (value === "DEL") {
+      // Delete the last character
+      currentValue = currentValue.slice(0, -1);
+    } else if (value === "RESET") {
+      // Reset the calculator
+      currentValue = "";
+    } else {
+      // Append the clicked value
+      currentValue += value;
+    }
+
+    // Update the display
+    calcDisplay.textContent = currentValue;
   });
+});
+
+var tmpAnswer = "";
+document.getElementById("compareBtn").addEventListener("click", function () {
+  tmpAnswer = calcDisplay.textContent;
+  tmpAnswer = tmpAnswer.slice(0, -1);
+  if (tmpAnswer.includes("+")) {
+    tmpAnswer = eval(tmpAnswer);
+    calcDisplay.innerText = tmpAnswer;
+    tmpAnswer = "";
+  } else if (tmpAnswer.includes("-")) {
+    tmpAnswer = eval(tmpAnswer);
+    calcDisplay.innerText = tmpAnswer;
+    tmpAnswer = "";
+  } else if (tmpAnswer.includes("*")) {
+    tmpAnswer = eval(tmpAnswer);
+    calcDisplay.innerText = tmpAnswer;
+    tmpAnswer = "";
+  } else if (tmpAnswer.includes("/")) {
+    tmpAnswer = eval(tmpAnswer);
+    calcDisplay.innerText = tmpAnswer;
+    tmpAnswer = "";
+  }
 });
