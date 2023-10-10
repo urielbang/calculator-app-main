b471acbf0d6a41ac19d4ac44c7dc40c8b5b10e74
urielbang <uri32655@gmail.com>
2023-10-10 13:07:18 +0300

    calculater done

--- main.js
+++ main.js
@@ -3,48 +3,81 @@ var collectionButtons = document.querySelectorAll("button");
 var calcDisplay = document.getElementById("displayCalc");
 
 var currentValue = "";
-var previousValue = "";
 
 collectionButtons.forEach((button) => {
-  button.addEventListener("click", function () {
+  button.addEventListener("click", () => {
     var value = button.textContent;
     if (value === "DEL") {
-      // Delete the last character
       currentValue = currentValue.slice(0, -1);
     } else if (value === "RESET") {
-      // Reset the calculator
       currentValue = "";
     } else {
-      // Append the clicked value
       currentValue += value;
     }
 
-    // Update the display
     calcDisplay.textContent = currentValue;
   });
 });
 
-var tmpAnswer = "";
-document.getElementById("compareBtn").addEventListener("click", function () {
-  tmpAnswer = calcDisplay.textContent;
-  tmpAnswer = tmpAnswer.slice(0, -1);
-  if (tmpAnswer.includes("+")) {
-    tmpAnswer = eval(tmpAnswer);
-    calcDisplay.innerText = tmpAnswer;
-    tmpAnswer = "";
-  } else if (tmpAnswer.includes("-")) {
-    tmpAnswer = eval(tmpAnswer);
-    calcDisplay.innerText = tmpAnswer;
-    tmpAnswer = "";
-  } else if (tmpAnswer.includes("*")) {
-    tmpAnswer = eval(tmpAnswer);
-    calcDisplay.innerText = tmpAnswer;
-    tmpAnswer = "";
-  } else if (tmpAnswer.includes("/")) {
-    tmpAnswer = eval(tmpAnswer);
-    calcDisplay.innerText = tmpAnswer;
-    tmpAnswer = "";
+compareBtn.addEventListener("click", () => {
+  let strTmp = calcDisplay.textContent;
+  let sum = 0;
+  let tmp = "";
+
+  if (strTmp.includes("+")) {
+    for (let i = 0; i < strTmp.length; i++) {
+      if ((strTmp[i] >= 0 && strTmp[i] <= 9) || strTmp[i] == "+") {
+        console.log(strTmp[i]);
+        tmp += strTmp[i];
+      }
+    }
+
+    strTmp = tmp.split("+");
+    console.log(strTmp);
+
+    for (let j = 0; j < strTmp.length; j++) {
+      sum += Number(strTmp[j]);
+    }
+    calcDisplay.textContent = sum;
+  } else if (strTmp.includes("-")) {
+    for (let i = 0; i < strTmp.length; i++) {
+      if ((strTmp[i] >= 0 && strTmp[i] <= 9) || strTmp[i] == "-") {
+        tmp += strTmp[i];
+      }
+    }
+
+    strTmp = tmp.split("-");
+
+    sum = strTmp[0] - strTmp[1];
+    for (let j = 2; j < strTmp.length; j++) {
+      sum = sum - strTmp[j];
+    }
+    calcDisplay.textContent = sum;
+  } else if (strTmp.includes("*")) {
+    for (let i = 0; i < strTmp.length; i++) {
+      if ((strTmp[i] >= 0 && strTmp[i] <= 9) || strTmp[i] == "*") {
+        tmp += strTmp[i];
+      }
+    }
+    strTmp = tmp.split("*");
+    sum = strTmp[0] * strTmp[1];
+    for (let j = 2; j < strTmp.length; j++) {
+      sum = sum * strTmp[j];
+    }
+    calcDisplay.textContent = sum;
+  } else if (strTmp.includes("/")) {
+    for (let i = 0; i < strTmp.length; i++) {
+      if ((strTmp[i] >= 0 && strTmp[i] <= 9) || strTmp[i] == "/") {
+        tmp += strTmp[i];
+      }
+    }
+    strTmp = tmp.split("/");
+    sum = parseFloat(strTmp[0]) / parseFloat(strTmp[1]);
+    for (let j = 2; j < strTmp.length; j++) {
+      sum = sum / parseFloat(strTmp[j]);
+    }
+    calcDisplay.textContent = sum;
   } else {
-    tmpAnswer = "wrong answer";
+    calcDisplay.textContent = "wrong number";
   }
 });
